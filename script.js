04ae8730165ba23f13a7f39e09c8d6d07d322652
StudentPM <studentpm@Administrators-MacBook-Pro.local>
2024-12-04 12:53:29 -0800

    Extra comments

--- script.js
+++ script.js
@@ -26,11 +26,13 @@ var operatorChosen = false;
 
 function settingNums(event){
     if (num1Chosen === false){
+        //If the first number hasnt been chosen, then it goes into num1
         num1 = (event.target.innerText);
         console.log(num1);
         display.innerText = num1;
     }
     if (operatorChosen === true){
+        //If the operator was chosen, goes into the num2
         num2 = (event.target.innerText);
         console.log(num2);
         display.innerText = num2;
@@ -38,7 +40,7 @@ function settingNums(event){
 }
 
 function settingOperator(event){
-
+    //Sets the operator as well as changes the condition
     setOperator = event.target.innerText;
     console.log(setOperator);
     display.innerText = setOperator;
@@ -52,27 +54,33 @@ function equation(){
     var result = 0;
 
     if (setOperator === "+"){
+        //Sets it to actual numbers
         result = parseInt(num1) + parseInt(num2);
         display.innerText = result;
     }
     if (setOperator === "-"){
+        //Sets it to actual numbers
         result = parseInt(num1) - parseInt(num2);
         display.innerText = result;
     }
     if (setOperator === "*"){
+        //Sets it to actual numbers
         result = parseInt(num1) * parseInt(num2);
         display.innerText = result;
     }
     if (setOperator === "/"){
+        //Sets it to actual numbers
         result = parseInt(num1) / parseInt(num2);
         if (num2 === 0){
+            //If the second number is a 0 while dividing, set to ERROR
             display.innerText = "ERROR";
         }
         else{
+            //If not, display the result
             display.innerText = result;
         }
     }
-
+    //changes the first one to the result
     num1 = result;
 
     console.log(result);
